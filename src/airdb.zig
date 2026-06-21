e4353d7a66f11fe75e81eb4badbfd12994ce48e0
Beau Ayres <DeveloperBeau@users.noreply.github.com>
2026-06-21 16:13:46 +1000

    Extract the durability Syncer interface into its own module

--- src/airdb.zig
+++ src/airdb.zig
@@ -16,14 +16,15 @@ pub const freelist = @import("freelist.zig");
 pub const Ref = @import("ref.zig").Ref;
 pub const node = @import("node.zig");
 pub const file_store = @import("file_store.zig");
+pub const syncer = @import("syncer.zig");
 pub const arena = @import("arena.zig");
 pub const slots = @import("slots.zig");
 pub const db = @import("db.zig");
 pub const Db = db.Db;
 pub const ReadTxn = db.ReadTxn;
 pub const WriteTxn = db.WriteTxn;
-pub const Syncer = @import("file_store.zig").Syncer;
-pub const FailingSyncer = @import("file_store.zig").FailingSyncer;
+pub const Syncer = @import("syncer.zig").Syncer;
+pub const FailingSyncer = @import("syncer.zig").FailingSyncer;
 
 test {
     @import("std").testing.refAllDecls(@This());
